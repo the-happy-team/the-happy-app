59459edc5493837d560eb8f415b2b0039e6d5b6e
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-11-26 11:09:16 -0300

    prevents initial redirections from home>sessions>camera when there are no sessions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+const { getAppPath } = require('electron').remote.app;
 const { ipcRenderer } = require('electron');
+const { existsSync, readdirSync } = require('fs');
 
 const { translate, setLanguage } = require('../js/translate');
+
+const pathJoin = require('path').join;
 
 window.onload = () => {
   ipcRenderer.send('restore-window');
@@ -11,4 +15,12 @@ window.onload = () => {
       setLanguage(b.getAttribute('data-language'));
     }, false);
   });
+
+  const feelingsPath = pathJoin(getAppPath(), 'feelings');
+  const indexButton = document.getElementById('my-index-button-link');
+
+  if((!existsSync(feelingsPath)) ||
+     (readdirSync(feelingsPath).filter(fn => !fn.startsWith('.')).length < 1)) {
+    indexButton.setAttribute('href', './camera.html');
+  }
 }
