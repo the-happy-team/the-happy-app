21320981fc7531f19660feb8f718a0677172348b
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-11-26 10:48:22 -0300

    translate index page

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const { translate, setLanguage } = require('../js/translate');
 
 window.onload = () => {
   ipcRenderer.send('restore-window');
+  translate();
+
   document.querySelectorAll('[data-language]').forEach(b => {
     b.addEventListener('click', () => {
       setLanguage(b.getAttribute('data-language'));
