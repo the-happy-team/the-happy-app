b8a233a162c4e46fa037e85708b900a0770c1126
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-10-13 12:11:30 -0400

    adds windows installer scripts

---
+++ installer/createWindowsInstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'release')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'the-happy-app-win32-ia32/'),
+    authors: 'The Happy Team',
+    noMsi: true,
+    outputDirectory: outPath,
+    exe: 'the-happy-app.exe',
+    setupExe: 'TheHappyAppInstaller.exe',
+    setupIcon: path.join(rootPath, 'icons', 'Happy.ico')
+  })
+}
